8404c2db26d72e82c94af267e0b08ebec8935b42
k-natori <k_natori@mac.com>
2026-03-15 20:50:06 +0900

    Changed html title

--- src/html.h
+++ src/html.h
@@ -6,7 +6,7 @@ const char *htmlPage = R"rawliteral(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EPD Image Upload - Method A</title>
+    <title>EPD Image Upload</title>
     <style>
         body {
             font-family: Arial, sans-serif;
